5bb0258588930c2a5c9370cb5efc3e18f73021f3
Islam Azab <islam.3azab@gmail.com>
2018-01-02 17:03:57 +0100

    modify default count to one

--- db/migrate/20180102153923_create_entries.rb
+++ db/migrate/20180102153923_create_entries.rb
@@ -2,7 +2,7 @@ class CreateEntries < ActiveRecord::Migration[5.1]
   def change
     create_table :entries do |t|
       t.string :name
-      t.integer :count, default: 0
+      t.integer :count, default: 1
 
       t.timestamps
     end
