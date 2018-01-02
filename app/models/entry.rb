39ac2ccd4dd557209aa30b833ca62eb83b6d3800
Islam Azab <islam.3azab@gmail.com>
2018-01-02 19:45:00 +0100

    add missing validations to entry model

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,7 @@
 require 'csv_import'
 class Entry < ApplicationRecord
   include CsvImport
+
+  validates :name, uniqueness: true, presence: true
+  validates :count, numericality: { greater_than: 0 }
 end
