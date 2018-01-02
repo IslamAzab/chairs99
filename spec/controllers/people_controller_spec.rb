1d9d680db1c7a849b6b253c7c4549aeabfb05873
Islam Azab <islam.3azab@gmail.com>
2018-01-02 22:26:50 +0100

    rewrote test

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -2,9 +2,22 @@ require 'rails_helper'
 
 RSpec.describe PeopleController, type: :controller do
   describe "POST import" do
-    it "accepts CSV file" do
+
+    before(:each) do
       csv_file = fixture_file_upload('files/single_repetition.csv', 'multipart/form-data')
       post :import, params: {file: csv_file}
+    end
+
+    it "should be successful" do
+      response.should be_success
+    end
+
+    it "accepts CSV file" do
+      parsed_body = JSON.parse(response.body)
+      expect(parsed_body.first["name"]).eql?("aaa")
+    end
+
+    it "returns a JSON object containing all first names which occur more than once and their count" do
       parsed_body = JSON.parse(response.body)
       expect(parsed_body.first["name"]).eql?("aaa")
     end
